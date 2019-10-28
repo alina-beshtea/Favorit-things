957510e8bcc159040d63aef4fb1143001a0f5a75
Student <example@example.com>
2019-10-28 10:51:20 -0700

    add push array and display it in html

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
   var answer1 =  $("#answer1").val();
   var answer2 =  $("#answer2").val();
   var answer3 =  $("#answer3").val();
-  var myArray = [answer1,answer2,answer3]
+  var originalArray = [answer1,answer2,answer3]
 
-  console.log(myArray);
+  originalArray.push("sam","jon","mary");
+  
+  document.write(originalArray);
+  // originalArray.display(ul);
+
+  console.log(originalArray);
 
 event.preventDefault();
   });
-//  var food Input = $(input#food).val();
-
-
 });
